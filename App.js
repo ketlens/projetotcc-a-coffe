22f36bea60acd626e3cd6fe2e30130a037ed4f05
Guilherme Ploeger Domingos <gulhermeploeger15@gmail.com>
2025-08-13 11:11:30 -0300

    alteração grazi

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Projeto teste</Text>
+      <Text>Projeto teste a</Text>
       <StatusBar style="auto" />
     </View>
   );
