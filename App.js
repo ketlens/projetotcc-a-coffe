61fd88b4134ba44b60f58a8664f7710a9253d5c4
Guilherme Ploeger Domingos <gulhermeploeger15@gmail.com>
2025-08-28 09:07:00 -0300

    [feat] Get user data

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import {useState} from 'react';
 
 export default function App() {
+  const [data, setData] = useState([])
+
+  const fetchData = async () => {
+    let URL = 'http://192.168.100.164:3000/api';
+    setLoading(true)
+    try {
+      const page = await fetch(`${URL}/user`);
+      if (!response.ok){
+        throw new Error(`Erro de servidor: ${response.status}`);
+      }
+      const json = await page.json();
+      setData(result);
+    } catch (error) {
+      console.log(error)
+      Alert.alert(error.message)
+    } finally{
+      setLoading(false)
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Projeto teste a</Text>
+      <Text>Projeto teste Coffe & Aroma</Text>
+        <View>
+          <Text>Dados do banco</Text>
+          {
+            data && data.map(item => {
+              return<Text>{item.username}</Text>
+            })
+          }
+          <Button title='buscar dados' onPress={fetchData}/>
+        </View>
+      
       <StatusBar style="auto" />
     </View>
   );
